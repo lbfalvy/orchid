ec022e810d5fb1662e5e6c6f35113646d1c5422f
Lawrence Bethlenfalvy <lbfalvy@protonmail.com>
2025-02-03 12:04:08 +0100

    slight job control tweak

--- xtask/src/orcx.rs
+++ xtask/src/orcx.rs
@@ -6,14 +6,13 @@ use crate::{Args, EXIT_OK};
 
 pub fn orcx(_args: &Args, subcommand: &[String]) -> io::Result<()> {
 	eprintln!("running orcx {}", subcommand.join(" "));
-	let status = Command::new("cargo").args(["build", "-p", "orchid-std"]).status()?;
-	if status.success() {
-		let status = Command::new("cargo")
-			.args(["run", "-p", "orcx", "--"].into_iter().chain(subcommand.iter().map(|s| s.as_str())))
-			.status()?;
-		EXIT_OK.store(status.success(), Ordering::Relaxed);
-	} else {
+	if !Command::new("cargo").args(["build", "-p", "orchid-std"]).status()?.success() {
 		EXIT_OK.store(false, Ordering::Relaxed);
+		return Ok(());
 	}
+	let status = Command::new("cargo")
+		.args(["run", "-p", "orcx", "--"].into_iter().chain(subcommand.iter().map(|s| s.as_str())))
+		.status()?;
+	EXIT_OK.store(status.success(), Ordering::Relaxed);
 	Ok(())
 }
